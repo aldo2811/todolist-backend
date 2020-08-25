70be6f7496198b9c3551e97b923850f3b2f816c1
Aldo Halim <aldohalim28@gmail.com>
2020-08-26 02:15:21 +0800

    [fix] datetime created not showing

--- todolist/models.py
+++ todolist/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
 
 
 # Create your models here.
@@ -31,11 +32,16 @@ class Task(models.Model):
     )
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
-    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_created = models.DateTimeField()
     datetime_due = models.DateTimeField()
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.datetime_created = timezone.now()
+        return super(Task, self).save(*args, **kwargs)
 
 
 @receiver(post_save, sender=User)
